5314716d2a65adf1bd681e96aa8fe72c26f849f4
Hiroyuki Yamada <mogwaing@gmail.com>
2026-05-19 18:22:23 +0900

    Fix Javadoc reference

--- core/src/main/java/com/scalar/db/saga/api/Named.java
+++ core/src/main/java/com/scalar/db/saga/api/Named.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
  *
  * <p>When only one resource of a given type is registered, {@code @Named} is not required.
  *
- * @see StepResolver
+ * @see com.scalar.db.saga.engine.StepResolver
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
